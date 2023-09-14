d885e479184fe3a11d466f40e0daad7b84248070
Nadim <nadimrifaii3@gmail.com>
2023-09-14 17:41:55 +0300

    x

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -39,7 +39,6 @@ let SignIn = () => {
       let response = await signInUser(email, password)
       setCurrentUser(response.user)
       setFields(defaultFields)
-      navigate('home')
     } catch (error) {
       console.log(error)
     }
