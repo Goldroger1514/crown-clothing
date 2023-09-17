1eaa27fe8ac29669968ad457050021ea3d736df1
Nadim <nadimrifaii3@gmail.com>
2023-09-17 12:28:16 +0300

    404.html

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import { UserProvider } from './context/user-context.component';
 import { CategoriesProvider } from './context/categories.context';
 import { CartProvider } from './context/cart-context';
