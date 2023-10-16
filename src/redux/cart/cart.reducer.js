915239974934e597efed99ea36b0e8d23ce8206f
Nadim <nadimrifaii3@gmail.com>
2023-10-16 19:14:36 +0300

    x

--- src/redux/cart/cart.reducer.js
+++ src/redux/cart/cart.reducer.js
@@ -1,4 +1,18 @@
 import { CART_TYPES } from "./cart.types";
 let INITIAL_STATE = {
-
+  cartItems: [],
+  itemsCount: 0,
+  total: 0,
+  cartOpen: false
+}
+export let cartReducer = (prevState = INITIAL_STATE, action) => {
+  let { type, payload } = action
+  switch (type) {
+    case CART_TYPES.ADD_TO_CART:
+      return {
+        ...prevState,
+        ...payload
+      }
+    default: return prevState
+  }
 }
